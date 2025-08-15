aa0069e98dc854377d59d4b8fc589ab9ffb9f23e
TecnohShinonome <TecnohShinonome@users.noreply.github.com>
2025-08-15 21:00:41 +0900

    font

--- client/main.lua
+++ client/main.lua
@@ -6,7 +6,7 @@ local RaceCount = 5
 
 function DrawText3Ds(x, y, z, text)
     SetTextScale(0.35, 0.35)
-    SetTextFont(4)
+    SetTextFont(0)
     SetTextProportional(1)
     SetTextColour(255, 255, 255, 215)
     BeginTextCommandDisplayText('STRING')
